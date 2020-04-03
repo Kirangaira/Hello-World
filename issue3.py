f576163c1b0461b4a4ea0cbde1d5d59f83082a73
Kirangaira <62208366+Kirangaira@users.noreply.github.com>
2020-04-03 21:47:01 +0530

    Delete issue3.py

--- issue3.py
+++
@@ -1,6 +0,0 @@
-from configparser import ConfigParser
-parser = ConfigParser()
-path = "issue3.config"
-parser.read(path)
-n = parser.get('RangeOfLoop', 'n')
-lst = [print("Hello World") for i in range(int(num))]
